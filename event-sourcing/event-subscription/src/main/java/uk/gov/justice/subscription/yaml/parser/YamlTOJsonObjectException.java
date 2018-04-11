f647784b1949ed4384a5731bb056434992b2630b
Mohamed Farouk <mohamed.farouk@hmcts.net>
2018-04-11 09:51:37 +0100

    Change subscription YAML to define event sources.

---
+++ event-sourcing/event-subscription/src/main/java/uk/gov/justice/subscription/yaml/parser/YamlTOJsonObjectException.java
@@ -0,0 +1,8 @@
+package uk.gov.justice.subscription.yaml.parser;
+
+public class YamlTOJsonObjectException extends RuntimeException {
+
+    public YamlTOJsonObjectException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
